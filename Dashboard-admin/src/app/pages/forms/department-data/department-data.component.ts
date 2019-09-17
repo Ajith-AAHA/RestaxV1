d63966347124de29c173b3e9b1d3c9ecd6b115ff
Your Name <ajithkumar@aahasolutions.com>
2019-09-17 15:55:52 +0530

    [changes]

--- Dashboard-admin/src/app/pages/forms/department-data/department-data.component.ts
+++ Dashboard-admin/src/app/pages/forms/department-data/department-data.component.ts
@@ -3,25 +3,27 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 
 @Component({
+  // tslint:disable-next-line: component-selector
   selector: 'nb-stepper-linear',
   templateUrl: './department-data.component.html',
-  styleUrls: ['./department-data.component.scss']
+  styleUrls: ['./department-data.component.scss'],
 })
 export class DepartmentDataComponent implements OnInit {
-  CardContent=['This page is used to manipulate your institues Department/Grades Database You can create,edit,delete a Department/Grades.'];
+  // tslint:disable-next-line: max-line-length
+  CardContent= ['This page is used to manipulate your institues  Department/Grades Database You can create,edit,delete a Department/Grades.'];
   elements: any = [
-    {id: 1, Coursename:'Bachelor of Technology',terms:'08',Duration:'4 years',Sectionsineachyear:'03,02,01'},
-    {id: 2, Coursename:'Master of Technology',terms:'06',Duration:'2 years',Sectionsineachyear:'02,02'},
-    {id: 3, Coursename:'Doctrate of Philosophy',terms:'02',Duration:'3 years',Sectionsineachyear:'01,01,01'},
+    {id: 1, Coursename: 'Bachelor of Technology', terms: '08', Duration: '4 years', Sectionsineachyear: '03,02,01'},
+    {id: 2, Coursename: 'Master of Technology', terms: '06', Duration: '2 years', Sectionsineachyear: '02,02'},
+    {id: 3, Coursename: 'Doctrate of Philosophy', terms: '02', Duration: '3 years', Sectionsineachyear: '01,01,01'},
   ];
 
-  headElements = ['ID', 'Course name', '#Terms', 'Duration','#Sections in each year'];
+  headElements = ['ID', 'Course name', '#Terms', 'Duration', '#Sections in each year'];
   tableHead = ['Department of Mechanical Engineering'];
-  stepperFirst:FormGroup;
-  stepperSecond:FormGroup;
+  stepperFirst: FormGroup;
+  stepperSecond: FormGroup;
   stepperThird: FormGroup;
 
-  constructor(private fb:FormBuilder) { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
     this.stepperFirst = this.fb.group({
@@ -37,7 +39,7 @@ export class DepartmentDataComponent implements OnInit {
   onFirstSubmit() {
     this.stepperFirst.markAsDirty();
   }
-  onSecondSubmit(){
+  onSecondSubmit() {
     this.stepperSecond.markAsDirty();
   }
   onThirdSubmit() {
